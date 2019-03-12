2c1ca6c4bd285d90c688d17c436a55edbf69f222
Sean Whalley <swhalley@gmail.com>
2019-03-12 18:54:21 -0300

    bootstrap information added

--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@ using Microsoft.Extensions.Logging;
 
 namespace dotnet_api_oauth
 {
+    //Application created from 
+    //dotnet new webapi -o appName
+    //Here is a good tutorial
+    //https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-2.2&tabs=visual-studio-code
+    //
+    //Additional dependencies added using. Following adds MYSQL Support to the application
+    //dotnet add package MySql.Data.EntityFrameworkCore --version 8.0.15
     public class Program
     {
         public static void Main(string[] args)
